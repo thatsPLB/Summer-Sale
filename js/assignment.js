671939ea43fb1f940b7fbb9b7a6a2b2439e227f8
Plabon Saha <sahaplabon66@gmail.com>
2023-08-20 23:38:22 +0600

    hash

--- js/assignment.js
+++ js/assignment.js
@@ -1,1 +1,36 @@
-console.log('hs')
+document.getElementById('prod1-click').addEventListener('click',function(){
+    console.log('shhs')
+    const janiNaField = document.getElementById('jani-na');
+    const janiNa = janiNaField.innerText;
+    console.log(janiNa)
+})
+document.getElementById('prod2-click').addEventListener('click',function(){
+    console.log('shhs')
+    const janiNaField2 = document.getElementById('jani-na-2');
+    const janiNa2 = janiNaField2.innerText;
+    console.log(janiNa2)
+})
+document.getElementById('prod3-click').addEventListener('click',function(){
+    console.log('shhs')
+    const janiNaField3 = document.getElementById('jani-na-3');
+    const janiNa3 = janiNaField3.innerText;
+    console.log(janiNa3)
+})
+document.getElementById('prod4-click').addEventListener('click',function(){
+    console.log('shhs')
+    const janiNaField4 = document.getElementById('jani-na-4');
+    const janiNa4 = janiNaField4.innerText;
+    console.log(janiNa4)
+})
+document.getElementById('prod5-click').addEventListener('click',function(){
+    console.log('shhs')
+    const janiNaField5 = document.getElementById('jani-na-5');
+    const janiNa5 = janiNaField5.innerText;
+    console.log(janiNa5)
+})
+document.getElementById('prod6-click').addEventListener('click',function(){
+    console.log('shhs')
+    const janiNaField6 = document.getElementById('jani-na-6');
+    const janiNa6 = janiNaField6.innerText;
+    console.log(janiNa6)
+})
